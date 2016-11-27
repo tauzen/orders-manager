6a19ff79ceb48e39ddafe695dbf439f4a1925e17
Krzysztof Mioduszewski <krzysiek@growbots.co>
2016-11-27 19:45:51 +0100

    Added simple consumer

--- orders_manager/queues.py
+++ orders_manager/queues.py
@@ -1,3 +1,8 @@
 from kombu import Exchange, Queue
 
 order_manager_ex = Exchange("order-manager", type="topic")
+shipments_q = Queue(
+    "shipments",
+    order_manager_ex,
+    routing_key="shipments"
+)
